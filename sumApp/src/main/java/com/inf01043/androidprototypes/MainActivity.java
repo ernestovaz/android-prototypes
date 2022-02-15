0339679c82ea0b737e679dbc723ceaae0841c0c3
ernesto <evoliveira@inf.ufrgs.br>
2022-02-15 10:34:32 -0300

    Create first app template

---
+++ sumApp/src/main/java/com/inf01043/androidprototypes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.inf01043.androidprototypes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
